8c1947b6b3d8fe12e52217873a2f6dc012ca7bf6
esbalabhakti-arch <esbalabhakti@gmail.com>
2025-11-25 21:22:57 -0800

    Add files via upload

---
+++ bphs-appRB-upto-Chap3Verse48/data.js
@@ -0,0 +1,465 @@
+// data.js
+// BPHS Flash Cards – Chapter 1 fully populated
+// You can later append more chapters to the `chapters` array.
+
+const chapters = [
+  //New Chapter begins
+  {
+    id: "ch0_1",
+    title: "Chapter 0 - Stories - Why BPHS? - 1",
+    description:
+      "Who is Sage Parashara? Who is Sage Maitreya",
+    audio: [
+      	{ label: "About Sage Parashara / Maitreya 1", file: "audio/Chapter0/Chap0_Podcast1.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch0_v1",
+        type: "Stories",
+        sanskrit:
+		"<img src='images/Chapter0/Chap0_Story1_PrMt_Fr.jpg' />",
+        translation:
+		"<img src='images/Chapter0/Chap0_Story1_PrMt_Fr.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+ 
+ //New Chapter begins
+    {
+    id: "ch0_2",
+    title: "Chapter 0 - Stories - Why BPHS? - 2",
+    description:
+      "Who is Sage Parashara? Who is Sage Maitreya",
+    audio: [
+      	{ label: "About Sage Parashara / Maitreya 2", file: "audio/Chapter0/Chap0_Podcast2.opus" }
+		],
+    cards: [
+      
+  {
+        id: "ch0_v2",
+        type: "Stories",
+        sanskrit:
+		"<img src='images/Chapter0/Chap0_Story1_PrMt_Fr.jpg' />",
+        translation:
+		"<img src='images/Chapter0/Chap0_Story1_PrMt_Fr.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+ 
+ //New Chapter begins
+    {
+    id: "ch0_3",
+    title: "Chapter 0 - Stories - Why BPHS? - 3",
+    description:
+      "Who is Sage Parashara? Who is Sage Maitreya",
+    audio: [
+      	{ label: "About Sage Parashara / Maitreya 3", file: "audio/Chapter0/Chap0_Podcast3.opus" }
+		],
+    cards: [
+      
+  {
+        id: "ch0_v3",
+        type: "Stories",
+        sanskrit:
+		"<img src='images/Chapter0/Chap0_Story1_PrMt_Fr.jpg' />",
+        translation:
+		"<img src='images/Chapter0/Chap0_Story1_PrMt_Fr.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+
+//New Chapter begins
+  {
+    id: "ch0_4",
+    title: "Chapter 0 - Śani & King Vikramāditya — The Story of Sade Sati",
+    description:
+      "Śani & King Vikramāditya — The Story of Sade Sati",
+    audio: [
+ 		{ label: "Śani & King Vikramāditya — The Story of Sade Sati", file: "audio/Chapter0/Chap0_Podcast4.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch0_v4",
+        type: "Stories",
+        sanskrit:
+		"<img src='images/Chapter0/Chap0_Sani_KingVikram_Fr.jpg' />",
+        translation:
+		"<img src='images/Chapter0/Chap0_Sani_KingVikram_Bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+  //Chapter Ends
+  
+ //New Chapter begins
+  {
+    id: "ch0_5",
+    title: "Chapter 0 - Chandra, Tara & the Birth of Budha (Mercury)",
+    description:
+      "Chandra, Tara & the Birth of Budha (Mercury)",
+    audio: [
+ 		{ label: "Chandra, Tara & the Birth of Budha (Mercury)", file: "audio/Chapter0/Chap0_Podcast5.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch0_v5",
+        type: "Stories",
+        sanskrit:
+		"<img src='images/Chapter0/Chap0_ChandraTaraBirthOfBudha_Fr.jpg' />",
+        translation:
+		"<img src='images/Chapter0/Chap0_ChandraTaraBirthOfBudha_Bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+ //Chapter Ends
+
+//New Chapter begins
+  {
+    id: "ch0_6",
+    title: "Chapter 0 - Rahu & Ketu — The Shadow Twins Who Chase the Sun and Moon",
+    description:
+      "Rahu & Ketu — The Shadow Twins Who Chase the Sun and Moon",
+    audio: [
+
+		{ label: "Rahu & Ketu — The Shadow Twins Who Chase the Sun and Moon", file: "audio/Chapter0/Chap0_Podcast6.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch0_v6",
+        type: "Stories",
+        sanskrit:
+		"<img src='images/Chapter0/Chap0_RahuKetu_Fr.jpg' />",
+        translation:
+		"<img src='images/Chapter0/Chap0_RahuKetu_Bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+   
+   //New Chapter begins
+  {
+    id: "ch1_1",
+    title: "Chapter 1 - Verses 1 to 3",
+    description:
+      "Maitreya humbly asking Parāśara to teach the supreme branch of Jyotiṣa — Hōra",
+    audio: [
+
+		{ label: "Chapter 1 verses 1 to 3", file: "audio/Chapter1/Chap1_v1_v3.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v1",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v1_v3_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v1_v3_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+   
+      //New Chapter begins
+  {
+    id: "ch1_2",
+    title: "Chapter 1 - Verses 4 to 6",
+    description:
+      "cosmology of Jyotiṣa—Maitreya asks about creation, and Parāśara invokes Brahman and the Sun before revealing the divine origins of this science.",
+    audio: [
+
+		{ label: "Chapter 1 verses 4 to 6", file: "audio/Chapter1/Chap1_v4_v6.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v2",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v4_v6_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v4_v6_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+   
+         //New Chapter begins
+  {
+    id: "ch1_3",
+    title: "Chapter 1 - Verses 7 to 9",
+    description:
+      "who deserves Jyotiṣa, warn against giving it to the unworthy, and root the teaching in the divine nature of Vishnu",
+    audio: [
+
+		{ label: "Chapter 1 verses 7 to 9", file: "audio/Chapter1/Chap1_v7_v9.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v3",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v7_v9_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v7_v9_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+
+          //New Chapter begins
+  {
+    id: "ch1_4",
+    title: "Chapter 1 - Verses 10 to 12",
+    description:
+      "Vishnu creates and sustains the universe through a single portion, with the remaining essence unmanifest and empowered by two divine shaktis",
+    audio: [
+
+		{ label: "Chapter 1 verses 10 to 12", file: "audio/Chapter1/Chap1_v10_v12.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v4",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v10_v12_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v10_v12_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+ 
+           //New Chapter begins
+  {
+    id: "ch1_5",
+    title: "Chapter 1 - Verses 13 to 15",
+    description:
+      "three divine śaktis and the expansions of Vishnu that form the foundation of cosmic creation",
+    audio: [
+
+		{ label: "Chapter 1 verses 13 to 15", file: "audio/Chapter1/Chap1_v13_v15.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v5",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v13_v15_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v13_v15_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+   
+           //New Chapter begins
+  {
+    id: "ch1_6",
+    title: "Chapter 1 - Verses 16 to 18",
+    description:
+      "Mahat, Ahaṅkāra, and the three guṇic evolutes arise from Vishnu’s divine expansions",
+    audio: [
+
+		{ label: "Chapter 1 verses 16 to 18", file: "audio/Chapter1/Chap1_v16_v18.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v6",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v16_v18_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v16_v18_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends   
+ 
+            //New Chapter begins
+  {
+    id: "ch1_7",
+    title: "Chapter 1 - Verses 19 to 21",
+    description:
+      "gods, senses, and elements arise from the guṇas and how Vishnu, Brahmā, and Śiva act through śakti while Paramātman pervades all beings.",
+    audio: [
+
+		{ label: "Chapter 1 verses 19 to 21", file: "audio/Chapter1/Chap1_v19_v21.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v7",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v19_v21_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v19_v21_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends   
+ 
+             //New Chapter begins
+  {
+    id: "ch1_8",
+    title: "Chapter 1 - Verses 22 to 24",
+    description:
+      "jīva and Paramātman portions vary among beings, planets, gods, and their śaktis.",
+    audio: [
+
+		{ label: "Chapter 1 verses 22 to 24", file: "audio/Chapter1/Chap1_v22_v24.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v8",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_v22_v24_fr.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_v22_v24_bk.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends  
+ 
+              //New Chapter begins
+  {
+    id: "ch1_9",
+    title: "Chapter 1 - Creation flow 1",
+    description:
+      "Creation flow",
+    audio: [
+
+		//{ label: "Chapter 1 verses 22 to 24", file: "audio/Chapter1/Chap1_v22_v24.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v9",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_creationflow_1.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_creationflow_1.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+ 
+               //New Chapter begins
+  {
+    id: "ch1_10",
+    title: "Chapter 1 - Creation flow 2",
+    description:
+      "Creation flow",
+    audio: [
+
+		//{ label: "Chapter 1 verses 22 to 24", file: "audio/Chapter1/Chap1_v22_v24.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v10",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_creationflow_2.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_creationflow_2.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+ 
+               //New Chapter begins
+  {
+    id: "ch1_11",
+    title: "Chapter 1 - Creation flow 3",
+    description:
+      "Creation flow",
+    audio: [
+
+		//{ label: "Chapter 1 verses 22 to 24", file: "audio/Chapter1/Chap1_v22_v24.opus" }
+    ],
+    cards: [
+      
+  {
+        id: "ch1_v11",
+        type: "Verses",
+        sanskrit:
+		"<img src='images/Chapter1/Chap1_creationflow_3.jpg' />",
+        translation:
+		"<img src='images/Chapter1/Chap1_creationflow_3.jpg' />",
+        wordByWord:
+    " " 
+      },
+    ]
+  },
+   //Chapter Ends
+ 
+ //New Chapter begins 
+ {
+id: "ch3_v46-v48",
+    title: "Chapter 3 – Graha-svabhāva-adhyāyaḥ - Verses 46to48",
+    description:
+      "Full nature of the grahas-qualities, forms, powers, and seasons - V46-48:seasonal rulerships, Ayurvedic classifications, and Saturn’s role as the giver of longevity ",
+    audio: [
+          { label: "Chapter 3 – Podcast Verse46-48", file: "audio//Chapter3/Chap3_Podcast_Verse46to48.opus" },
+    ],
+    cards: [
+      {
+        id: "ch3_v46-48",
+        //type: "verse",
+        //verseNumber: 46,
+        sanskrit:
+		"<img src='images/Chapter3/348fr_Chap3_Verse46to48.jpg' />",
+        //hint: "Opening scene – who bows and to whom?",
+        translation:
+		"<img src='images/Chapter3/348bk_Chap3_Verse46to48.jpg' />",
+        wordByWord:
+" "
+      }
+]
+}
+ //Chapter Ends
+];
